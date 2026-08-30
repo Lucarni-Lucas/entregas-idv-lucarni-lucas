c3cc92a29dee73afcdef45c29b947c4f92cbeeba
Lucas Nahuel Lucarni <lucaslucarni@yahoo.com.ar>
2026-08-30 19:48:45 -0300

    feature: implementacion mruv de player

--- entrega-2-lucarni-lucas/desafio-2/entrega-2-desafio-2-lucarni-lucas/entities/player/player.gd
+++ entrega-2-lucarni-lucas/desafio-2/entrega-2-desafio-2-lucarni-lucas/entities/player/player.gd
@@ -2,23 +2,27 @@ extends Sprite2D
 
 @onready var cannon:Sprite2D = $Cannon
 
-@export var speed: float = 200.0
+const ACCELERATION: float = 20.0
+const H_SPEED_LIMIT: float = 400.0
+const FRICTION_WEIGHT: float = 0.2
 
 var projectile_container:Node
 
-var velocity
+var velocity: Vector2 = Vector2.ZERO
 
 func set_projectile_container(container:Node):
 	cannon.projectile_container = container
 	self.projectile_container = container
 
 
-
 func _physics_process(delta: float) -> void:
 	# Movimiento
-	var direction := Input.get_axis("move_left", "move_right")
-	position.x += direction * speed * delta
-	
+	var h_movement_direction:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	if h_movement_direction != 0:
+		velocity.x = clampf(velocity.x + (h_movement_direction * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
+	else:
+		velocity.x = lerpf(velocity.x, 0, FRICTION_WEIGHT) if absf(velocity.x) > 1 else 0
+	position += velocity * delta
 	
 	# Cannon
 	var mouse_position:Vector2 = get_global_mouse_position()
